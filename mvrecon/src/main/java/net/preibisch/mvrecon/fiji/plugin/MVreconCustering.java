8f3a0739f9d596c1bc27e84a2ba00da4d1d6ebc6
Marwan <zouinkhi.marwan@gmail.com>
2020-01-20 10:03:43 +0100

    adapt to distribution

--- mvrecon/src/main/java/net/preibisch/mvrecon/fiji/plugin/MVreconCustering.java
+++ mvrecon/src/main/java/net/preibisch/mvrecon/fiji/plugin/MVreconCustering.java
@@ -4,25 +4,27 @@ import java.util.List;
 
 import mpicbg.spim.data.sequence.ViewDescription;
 import net.imglib2.Interval;
-import net.preibisch.distribution.algorithm.task.TaskType;
+import net.preibisch.distribution.algorithm.clustering.scripting.TaskType;
 import net.preibisch.distribution.headless.Clustering;
-import net.preibisch.distribution.tools.Tools;
+import net.preibisch.distribution.tools.helpers.IOHelpers;
 import net.preibisch.mvrecon.fiji.plugin.fusion.FusionGUI;
+import net.preibisch.mvrecon.fiji.spimdata.boundingbox.BoundingBox;
 import net.preibisch.mvrecon.process.interestpointregistration.pairwise.constellation.grouping.Group;
 
 public class MVreconCustering {
 
 	public static void run(FusionGUI fusion) {
-		int task = 0;
+		TaskType task ;
 		if (fusion.getNonRigidParameters().isActive())
 			task = TaskType.NON_RIGID;
 		else
 			task = TaskType.FUSION;
-		String xml = Tools.getXML(fusion.getSpimData().getBasePath().getAbsolutePath());
+		String xml = IOHelpers.getXML(fusion.getSpimData().getBasePath().getAbsolutePath());
 		System.out.println("XML: "+xml);
 		double downsampling = fusion.getDownsampling();
 		System.out.println("downsampling: "+downsampling);
-		Interval interval = fusion.getBoundingBox();
+		BoundingBox interval = (BoundingBox) fusion.getBoundingBox();
+		
 		System.out.println("interval: "+interval.toString());
 		List<Group<ViewDescription>> groups = fusion.getFusionGroups();
 		
